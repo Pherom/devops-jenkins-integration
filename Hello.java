47b5a302eb325138c57dac8d3edd65f8336e00e5
Pherom <pheromem@gmail.com>
2023-04-16 18:38:57 +0300

    Edited Hello.java file after Jenkins integration

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         for (int i = 2; i <= 10; i++) {
             System.out.println("Hello World ..." + i);
+            System.out.println("This is pushed after Jenkins integration ..." + i);
         }
     }
 }
